4283e3dc942eb5b39ad380fb92dac571dc28ee3e
a27993 <a27993@alunos.ipca.pt>
2024-10-21 15:04:01 +0100

    Inicio

---
+++ GereTurismo/Clientes/Class1.cs
@@ -0,0 +1,74 @@
+﻿//-----------------------------------------------------------------
+//    <copyright file="Clientes.cs" company="IPCA">
+//     Copyright IPCA-EST. All rights reserved.
+//    </copyright>
+//    <date>20-10-2024</date>
+//    <time>19:00</time>
+//    <version>0.1</version>
+//    <author>Hugo Monteiro</author>
+//-----------------------------------------------------------------
+using System;
+
+namespace Clientes
+{
+	public class cliente
+	{
+		#region Private
+		private string nome { get; set; }
+		private string cc { get; set; }
+		private string email { get; set; }
+		private string telemovel { get; set; }
+		private string nif { get; set; }
+		private int id { get; set; }
+		#endregion
+
+		#region Public
+		public int Id
+		{
+			get { return this.id; }
+			set { this.id = value; }
+		}
+		public string Name
+		{
+			get { return this.nome; }
+			set { this.nome = value; }
+		}
+
+		public string CC
+		{
+			get { return this.cc; }
+			set { this.cc = value; }
+		}
+
+		public string Email
+		{
+			get { return this.email; }
+			set { this.email = value; }
+		}
+
+		public string Telemovel
+		{
+			get { return this.telemovel; }
+			set { this.telemovel = value; }
+		}
+
+		public string Nif
+		{
+			get { return this.nif; }
+			set { this.nif = value; }
+		}
+		#endregion
+
+		#region Constructors
+		public cliente()
+		{
+			nome = "";
+			email = "";
+			cc = "";
+			nif = "";
+			telemovel = "";
+			id = 0;
+		}
+		#endregion
+	}
+}
---
+++ GereTurismo/Main/Program.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------
+//    <copyright file="Main.cs" company="IPCA">
+//     Copyright IPCA-EST. All rights reserved.
+//    </copyright>
+//    <date>20-10-2024</date>
+//    <time>19:00</time>
+//    <version>0.1</version>
+//    <author>Hugo Monteiro</author>
+//-----------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
